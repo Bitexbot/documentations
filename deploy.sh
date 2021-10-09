cb7577add877da46d96fc11fa3f4e4380a54af87
Bitexbot <bitexbot@gmail.com>
2021-10-09 09:45:26 +0200

    remove unused deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Token remain valid for 30 days. If something wrong generate new one here
-# https://github.com/settings/tokens
-set -o allexport; 
-source .env; 
-set +o allexport;
-yarn deploy;
-sleep 10m;
